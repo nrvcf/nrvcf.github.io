52e0bf7e6e82a94cfd1dd6d1064974b464dafe83
Brad Gignac <brad.gignac@rackspace.com>
2012-01-30 14:08:46 -0500

    Switch compass output to expanded for development.

--- config.rb
+++ config.rb
@@ -2,4 +2,4 @@ css_dir = "assets/stylesheets"
 images_dir = "assets/images"
 javascripts_dir = "assets/javascripts"
 sass_dir = "assets/stylesheets/source"
-output_style = :compressed
+output_style = :expanded
