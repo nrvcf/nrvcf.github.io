95610277033af3745889cdc6cc2cc3ef5b7f5b77
Brad Gignac <bgignac@bradgignac.com>
2012-02-05 14:46:21 -0500

    Updated compass config so output is compressed.

--- config/compass.rb
+++ config/compass.rb
@@ -1,5 +1,6 @@
+project_path = "app"
 css_dir = "assets/stylesheets"
 images_dir = "assets/images"
 javascripts_dir = "assets/javascripts"
 sass_dir = "assets/stylesheets/source"
-output_style = :expanded
+output_style = :compressed
